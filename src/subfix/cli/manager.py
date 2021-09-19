1c7217e0fcdcdceada4b28c8f5ebffcd474dc14b
Manicarus <swkang7229@gmail.com>
2021-09-19 23:01:18 +0900

    `source` is no longer positional argument
    `subfix` default action:
      - takes all `.srt` subtitles for processing in the current working
        directory
      - puts all processed subtitles into `patched` directory

--- src/subfix/cli/manager.py
+++ src/subfix/cli/manager.py
@@ -2,12 +2,24 @@ from subfix.converter.manager import converter_manager
 import argparse
 import sys, os
 
+DEFAULT_TARGET_DIRNAME = 'patched'
+
 def main():
+    
+    default_source_path = os.getcwd()
+    default_target_path = os.path.join(default_source_path, 
+        DEFAULT_TARGET_DIRNAME)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('source', nargs='+', help='source directory or filename')
-    parser.add_argument('-t', '--target', nargs='?', help='target directory or filename')
+    parser.add_argument('-s', '--source', nargs='*', 
+        default=[default_source_path], 
+        help='source directory or filename (default=<current working directory>)')
+    parser.add_argument('-t', '--target', nargs='?', 
+        default=default_target_path, 
+        help='target directory or filename')
     parser.add_argument('-x', '--extension', nargs='*', 
-        default='srt', help='list of file extensions you want to process (default=srt)')
+        default=['srt'], 
+        help='list of file extensions you want to process (default=srt)')
     args = parser.parse_args()
     
     # print(args)
